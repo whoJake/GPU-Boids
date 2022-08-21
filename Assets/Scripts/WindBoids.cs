f7b939468c7d9c71511fb4aca73f217dc9fbe21c
Jake Cooper <37589250+JakeUK@users.noreply.github.com>
2022-08-21 16:04:42 +0100

    Boid information formatted properly into texture
    r = vel.normalized.x
    g = vel.normalized.y
    b = vel.magnitude
    a = strength (distance from boid and fading)

--- Assets/Scripts/WindBoids.cs
+++ Assets/Scripts/WindBoids.cs
@@ -11,7 +11,8 @@ public class WindBoids : MonoBehaviour
 
     public Vector2Int outputTextureSize;
     public RawImage outputImage;
-    public bool clearTrail;
+    [Range(0, 5)]
+    public int drawRadius;
 
     [Header("Boid Settings")]
     [Min(1)]
@@ -38,8 +39,8 @@ public class WindBoids : MonoBehaviour
     public float maxSteerForce;
 
     [Header("Processing Settings")]
-    [Range(0f, 1f)]
-    public float fadeSpeed;
+    [Range(1f, 10f)]
+    public float fadeSpeed = 1f;
     #endregion
 
     #region Internal Variables
@@ -47,9 +48,11 @@ public class WindBoids : MonoBehaviour
     #endregion
 
     #region Compute Shader Variables
-    private RenderTexture outputBoidTexture;
     private ComputeShader boidCompute;
     private int[] computeDim;
+
+    private ComputeShader windBuilder;
+    private RenderTexture windTexture;
     #endregion
 
     #region Compute Helpers
@@ -68,15 +71,12 @@ public class WindBoids : MonoBehaviour
 
         public int numFlockmates;
 
-        public Vector4 color;
-
         public static int Size {
             get {
                 return (sizeof(int))
                     + (sizeof(float) * 2 * 2)
                     + (sizeof(float) * 2 * 3)
-                    + (sizeof(int))
-                    + (sizeof(float) * 4);
+                    + (sizeof(int));
             }
         }
     }
@@ -84,17 +84,18 @@ public class WindBoids : MonoBehaviour
     void OnEnable() {
         #region Load Resources
         boidCompute = Resources.Load<ComputeShader>("WindBoidsCompute");
+        windBuilder = Resources.Load<ComputeShader>("WindBuilder");
         clearTextureCompute = Resources.Load<ComputeShader>("ClearTexture");
         #endregion
     }
 
     void Start() {
-        outputBoidTexture = new RenderTexture(outputTextureSize.x, outputTextureSize.y, 0);
-        outputBoidTexture.filterMode = FilterMode.Point;
-        outputBoidTexture.enableRandomWrite = true;
-        outputBoidTexture.Create();
+        windTexture = new RenderTexture(outputTextureSize.x, outputTextureSize.y, 0);
+        windTexture.filterMode = FilterMode.Point;
+        windTexture.enableRandomWrite = true;
+        windTexture.Create();
 
-        outputImage.texture = outputBoidTexture;
+        outputImage.texture = windTexture;
 
         //Perform initial setup
         SetUpBoids();
@@ -109,25 +110,30 @@ public class WindBoids : MonoBehaviour
             boidHandler[i] = new Boid {
                 group = 0,
                 position = new Vector2(Random.Range(0f, outputTextureSize.x), Random.Range(0f, outputTextureSize.y)),
-                velocity = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized * ((maxSpeed - minSpeed) / 2),
-                color = Color.green
+                velocity = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized * ((maxSpeed - minSpeed) / 2)
             };
         }
-
-        var boidBuffer = new ComputeBuffer(numberOfBoids, Boid.Size);
-        boidBuffer.SetData(boidHandler);
-        Shader.SetGlobalBuffer("_BoidData", boidBuffer);
     }
 
     void FixedUpdate() {
+
+        RenderTexture lastFrame = new RenderTexture(outputTextureSize.x, outputTextureSize.y, 0);
+        lastFrame.Create();
+        Graphics.CopyTexture(windTexture, lastFrame);
+
         //Clear agent map
-        if (clearTrail) ClearTexture(outputBoidTexture);
+        ClearTexture(windTexture);
+
 
         //Setup compute variables
         int numOfBatches = Mathf.CeilToInt(numberOfBoids / 8);
 
+
+        var boidBuffer = new ComputeBuffer(numberOfBoids, Boid.Size);
+        boidBuffer.SetData(boidHandler);
+
         //Set compute variables
-        boidCompute.SetTexture(0, "_BoidMap", outputBoidTexture);
+        boidCompute.SetBuffer(0, "_BoidData", boidBuffer);
         boidCompute.SetInts("_TextureDimensions", computeDim);
         boidCompute.SetInt("_BoidCount", numberOfBoids);
         boidCompute.SetFloat("_DetectionDistance", detectionDistance);
@@ -144,6 +150,26 @@ public class WindBoids : MonoBehaviour
         boidCompute.SetFloat("_SeperationWeight", seperationWeight);
 
         boidCompute.Dispatch(0, numOfBatches, 1, 1);
+        boidBuffer.GetData(boidHandler);
+        boidBuffer.Release();
+
+        var outputBoids = new ComputeBuffer(numberOfBoids, Boid.Size);
+        outputBoids.SetData(boidHandler);
+
+        //Draw and build wind
+        windBuilder.SetTexture(0, "_WindOutput", windTexture);
+        windBuilder.SetTexture(0, "_LastFrame", lastFrame);
+        windBuilder.SetBuffer(0, "_BoidInput", outputBoids);
+        windBuilder.SetFloat("_MaxMagnitude", maxSpeed);
+        windBuilder.SetFloat("_MinMagnitude", minSpeed);
+        windBuilder.SetInts("_TextureDimensions", computeDim);
+        windBuilder.SetFloat("_DrawRadius", (float)drawRadius);
+
+
+        windBuilder.Dispatch(0, numOfBatches, 1, 1);
+        outputBoids.Release();
+
+        lastFrame.Release();
     }
 
 
@@ -154,6 +180,7 @@ public class WindBoids : MonoBehaviour
         clearTextureCompute.SetTexture(0, "_Texture", rt);
         clearTextureCompute.SetFloat("width", rt.width);
         clearTextureCompute.SetFloat("height", rt.height);
+        clearTextureCompute.SetFloat("_FadeSpeed", fadeSpeed);
 
         clearTextureCompute.Dispatch(0, numBatchX, numBatchY, 1);
     }
